e2bf0bc32f95148cf39d6c0e7fb961113eb59f96
Bhavesh Odedra <nisha.b.odedra@gmail.com>
2018-01-19 14:40:17 +0530

    [ADD] list price by delivery address

---
+++ __openerp__.py
@@ -0,0 +1,15 @@
+# -*- encoding: utf-8 -*-
+
+{
+    "name": "List Price by Delivery Address",
+    "version": "8.0.0.1.0",
+    "author": "Bhavesh Odedra",
+    "website": "",
+    "category": "Sales",
+    "depends": [
+        "sale",
+    ],
+    "data": [
+    ],
+    "installable": True
+}
---
+++ models/sale.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    def onchange_delivery_id(self, cr, uid, ids, company_id, partner_id,
+        delivery_id, fiscal_position, context=None):
+        res = super(SaleOrder, self).onchange_delivery_id(
+            cr, uid, ids, company_id, partner_id,
+            delivery_id, fiscal_position, context=context)
+        if delivery_id:
+            partner = self.pool.get('res.partner').browse(
+                cr, uid, delivery_id, context=context)
+            if partner.property_product_pricelist:
+                res['value']['pricelist_id'] =\
+                    partner.property_product_pricelist.id
+        return res
+
+    def onchange_partner_id(self, cr, uid, ids, part, context=None):
+        res = super(SaleOrder, self).onchange_partner_id(
+            cr, uid, ids, part, context=context)
+        if part:
+            addr = self.pool.get('res.partner').address_get(
+                cr, uid, [part], ['delivery', 'invoice', 'contact'])
+            delivery_onchange = self.onchange_delivery_id(
+                cr, uid, ids, False, part,
+                addr['delivery'], False,  context=context)
+            if res and res['value']:
+                res['value'].update(delivery_onchange['value'])
+        return res
